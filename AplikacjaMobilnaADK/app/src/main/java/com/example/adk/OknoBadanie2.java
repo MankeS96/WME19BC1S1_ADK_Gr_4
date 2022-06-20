103af5eb8eead3564a9b1b55ad7416eb4d0b09fb
grelewska <87614950+grelewska@users.noreply.github.com>
2022-06-20 13:13:04 +0200

    Edycja przeprowadzenia badania
    Dodano timer, status nagrania, brak zapisu pliku w telefonie

--- AplikacjaMobilnaADK/app/src/main/java/com/example/adk/OknoBadanie2.java
+++ AplikacjaMobilnaADK/app/src/main/java/com/example/adk/OknoBadanie2.java
@@ -1,14 +1,18 @@
 package com.example.adk;
 
-import android.Manifest;
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Environment;
-import android.os.SystemClock;
 import android.util.Log;
 import android.view.WindowManager;
-import android.widget.Chronometer;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,122 +22,144 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
-import java.util.Locale;
-import java.util.logging.SimpleFormatter;
 
-public class OknoBadanie2 extends AppCompatActivity{
+public class OknoBadanie2 extends AppCompatActivity {
 
-    // constant for storing audio permission
-    private static final int REQUEST_AUDIO_PERMISSION_CODE=1;
-
-    // Initializing all variables..
-    ImageView  recordB, grafika;
-    TextView komunikatBadanie2;
-    Chronometer chronometer;
-
+    Animation topAnim, bottomAnim;
+    ImageView recordB, grafika;
+    TextView komunikatBadanie2, statusTV3, statusTV;
+    private MediaRecorder mRecorder;
+    private long timeLeftInMilliseconds = 30000; //30 sek
+    public static final int REQUEST_AUDIO_PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_okno_badanie2);
 
-        //Hooks
         recordB = findViewById(R.id.recordButton);
         grafika = findViewById(R.id.naglowek_oknoBadanie2);
         komunikatBadanie2 = findViewById(R.id.komunikatBadanie2);
-        chronometer = findViewById(R.id.record_Timer);
+        statusTV3 = findViewById(R.id.idTVstatus3);
+        statusTV = findViewById(R.id.idTVstatus);
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
 
-        recordB.setOnClickListener(view -> {
-            // start recording method will
-            // start the recording of audio.
-            startRecording();
-            recordB.setImageDrawable(ContextCompat.getDrawable(OknoBadanie2.this,R.drawable.record2));
+        komunikatBadanie2.setAnimation(topAnim);
+        //grafika.setAnimation(topAnim);
+        statusTV.setAnimation(bottomAnim);
+        statusTV3.setAnimation(bottomAnim);
+        recordB.setAnimation(bottomAnim);
 
-        });
+        recordB.setOnClickListener(view -> startRecording());
+
+
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+        Log.d("Files", "Path: " + path);
+        File directory = new File(path);
     }
 
+    private String getNameByDateTime() {
+
+        Date date = Calendar.getInstance().getTime();
+        @SuppressLint("SimpleDateFormat") DateFormat dateFormat = new SimpleDateFormat("yyMMdd_hhmmss");
+        //String strDate = "Audio_"; // + dateFormat.format(date);
+        return "A_" + dateFormat.format(date);
+    }
+
+    @SuppressLint("SetTextI18n")
     private void startRecording() {
-        // check permission method is used to check
-        // that the user has granted permission
-        // to record nd store the audio.
-        if (checkRecordingPermission()) {
-
-            // we are here initializing our filename variable
-            // with the path of the recorded audio file.
-            // string variable is created for storing a file name
+        if (CheckPermissions()) {
             String mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
-            mFileName = mFileName + "/AudioRecording.3gp";
+            mFileName += "/" + getNameByDateTime() + ".3gp";
 
-            // below method is used to initialize
-            // the media recorder class
-            // creating a variable for media recorder object class.
-            MediaRecorder mRecorder = new MediaRecorder();
-
-            // below method is used to set the audio
-            // source which we are using a mic.
+            mRecorder = new MediaRecorder();
             mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-
-            // below method is used to set
-            // the output format of the audio.
             mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-
-            // below method is used to set the
-            // audio encoder for our recorded audio.
             mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-
-            // below method is used to set the
-            // output file location for our recorded audio
             mRecorder.setOutputFile(mFileName);
-
-            chronometer.setBase(SystemClock.elapsedRealtime());
             try {
-                // below method will prepare
-                // our audio recorder class
                 mRecorder.prepare();
             } catch (IOException e) {
                 Log.e("TAG", "prepare() failed");
             }
-            // start method will start
-            // the audio recording.
             mRecorder.start();
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss", Locale.CANADA); //zmienic na polska
-            Date now = new Date();
-            chronometer.start();
+            startTimer();
+            recordB.setImageDrawable(getResources().getDrawable(R.drawable.record2));
+            statusTV.setText("Rozpoczęto pomiar nr 1");
 
         } else {
-            // if audio recording permissions are
-            // not granted by user below method will
-            // ask for runtime permission for mic and storage.
-            requestRecordingPermission();
+            RequestPermissions();
         }
     }
 
-    private void requestRecordingPermission(){
-        ActivityCompat.requestPermissions(OknoBadanie2.this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_AUDIO_PERMISSION_CODE);
-    }
-
-    public boolean checkRecordingPermission(){
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)== PackageManager.PERMISSION_DENIED){
-            requestRecordingPermission();
-            return false;
-        }
-        return false;
+    @SuppressLint("SetTextI18n")
+    public void pauseRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+        recordB.setImageDrawable(getResources().getDrawable(R.drawable.record));
+        statusTV.setText("Zakończono pomiar nr 1");
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode==REQUEST_AUDIO_PERMISSION_CODE){
-            if(grantResults.length>0){
-                boolean permissionToRecord = grantResults[0]==PackageManager.PERMISSION_GRANTED;
-                if (!permissionToRecord){
-                    Toast.makeText(getApplicationContext(),"Odmowa dostępu do mikrofonu", Toast.LENGTH_SHORT).show();
+        if (requestCode == REQUEST_AUDIO_PERMISSION_CODE) {
+            if (grantResults.length > 0) {
+                boolean permissionToRecord = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean permissionToStore = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                if (permissionToRecord && permissionToStore) {
+                    Toast.makeText(getApplicationContext(), "Przyznano uprawnienia", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Odrzucono uprawnienia", Toast.LENGTH_LONG).show();
                 }
             }
         }
     }
+
+    public void startTimer(){
+        CountDownTimer countDownTimer = new CountDownTimer(timeLeftInMilliseconds, 1000) {
+            @Override
+            public void onTick(long l) {
+                timeLeftInMilliseconds = l;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+                pauseRecording();
+            }
+        }.start();
+
+    }
+
+    public void updateTimer(){
+        int minutes = (int) timeLeftInMilliseconds /60000;
+        int seconds = (int) timeLeftInMilliseconds %60000/1000;
+        String timeLeftText;
+        timeLeftText = "" +minutes;
+        timeLeftText += ":";
+        if (seconds<10) timeLeftText += "0";
+        timeLeftText += seconds;
+        statusTV3.setText(timeLeftText);
+    }
+
+    public boolean CheckPermissions() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void RequestPermissions() {
+        ActivityCompat.requestPermissions(OknoBadanie2.this, new String[]{RECORD_AUDIO, WRITE_EXTERNAL_STORAGE}, REQUEST_AUDIO_PERMISSION_CODE);
+    }
+
 }
